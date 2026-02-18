d545acb285d148dd6892599c6144790803ba89e1
aabdul1300 <abdulmananar2005@gmail.com>
2026-02-17 21:46:23 -0800

    Update SoftwareStore.cpp
    assignment 2

--- SoftwareStore.cpp
+++ SoftwareStore.cpp
@@ -3,14 +3,54 @@
 using namespace std;
 int main()
 {
-    double mph = 6.5;
-    // PACE calculation
-    /* write your code */
 
-    //express pace in minutes and seconds
-  /* write your code here */
+    const double RETAIL_PRICE = 199.00;
+    int quantity;
+    double discountRate = 0.0;
 
-  //express your output in 1 decimal points
+    cout << "Enter number of units purchased: ";
+    cin >> quantity;
 
+    if (quantity <= 0) {
+        cout << "Quantity must be greater than 0. " << endl;
+        return 1;
+
+    }
+
+    if (quantity >= 100) {
+        discountRate = 0.50;
+
+    }
+    else if (quantity >= 50) {
+        discountRate = 0.40;
+    }
+
+    else if (quantity >= 20) {
+        discountRate = 0.30;
+    }
+
+    else if (quantity >= 10) {
+        discountRate = 0.20;
+    }
+
+    else {     
+        discountRate = 0.0; 
+         }
+    
+    double subtotal = quantity * RETAIL PRICE;
+    double totalCost = subtotal * (1.0 - discountRate);
+
+    cout << fixed << setprecision(2);
+    cout << "Subtotal: $" << subtotal << endl;
+    cout << "Discount Rate: " << discountRate * 100 << "%" << endl;
+    cout << "Total Cost: $" << totalCost << endl;
+
+
+
+
+
+
+
+    return 0;
 
 }
